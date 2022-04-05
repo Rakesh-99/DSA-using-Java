f5d2d9d0420c25b7fa25f71d7d9d4e9b01689ae4
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-04-06 01:37:01 +0530

    Query on Linear Search Algorithm

--- LinearSearch/src/LinearSearchDemo.java
+++ LinearSearch/src/LinearSearchDemo.java
@@ -1,7 +1,36 @@
+
+import java.util.Scanner;;
+
+
 public class LinearSearchDemo
 {
 	public static void main(String[] args)
 	{
 		
+		
+		
+		int[] arr = {23,11,44,21,1,5,3,9,26,41};
+		
+		int findNum =41;
+		
+		int search = linearSearch(arr, findNum);
+		
+		System.out.println(search);
+		
 	}
+	
+	public static int linearSearch(int[] arr, int findNum)
+	{
+		
+		for(int i =0 ; i < arr.length; i++)
+		{
+			if(arr[i] == findNum)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
 }
+	
